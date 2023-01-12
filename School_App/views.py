4ed757222f5d81bdb4062eb5afe0f1f8ccbddcf5
brijeshraiyani <brijeshraiyani20@gmail.com>
2023-01-12 15:44:48 +0530

    setting change , models , views

--- School_App/views.py
+++ School_App/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
+from School_App.models import User,UserType
 
 # Create your views here.
 
 def signup(request):
     if request.method == "POST":
-        fname = request.post['fname']
-        mname = request.post['mname']
+        fname = request.POST['fname']
+        mname = request.POST['mname']
+        lname = request.POST['lname']
+        number = request.POST['number']
+        mail = request.POST['email']
+        standard = request.POST['standard']
+        file = request.FILE['file']
+
         password = request.post['password']
         cpassword = request.post['cpassword']
-        if(password == cpassword):
-            pass
+
+        try:
+            userobj = User.objects.get(email=mail)
+            msg = "You are Already register with this email id"
+            context = {'msg':msg}
+            return render(request,"login.html",context)
+        except:        
+            if(password == cpassword):
+                pass
     else:
         return render(request,"signup.html")
 
